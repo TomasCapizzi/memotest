ab713a3b9ecf5fcfa1be44e380d342368af428ad
TomasCapizzi <tomas.capizzi@hotmail.com>
2021-06-08 22:37:44 -0300

    Actualizacion funcionalidades

--- codigo.js
+++ codigo.js
@@ -1,8 +1,18 @@
 'use strict';
 
 const contenedor = document.getElementById('contenedor');
-const resultado = document.getElementById('resultado');
-const btnPlay = document.getElementById('play')
+const tablero = document.getElementById('tablero');
+const pantallaInicio = document.getElementById('inicio')
+const Modal = document.getElementById('modal-contenedor');
+
+
+
+//  Botones de Dificultad  //
+const btnFacil = document.getElementById('btn-facil');
+const btnIntermedio = document.getElementById('btn-medio');
+const btnDificil = document.getElementById('btn-dificil');
+
+////////////////////////////////////////////////////////////
 
 const Fichas = [{
     id: 1,
@@ -42,18 +52,62 @@ const Fichas = [{
     color: 'rosa',
 }];
 
+//////// Eventos de botones  ////////////
+let movimientos = 0;
+btnFacil.addEventListener('click', () => {
+    movimientos = 26;
+    pantallaInicio.innerHTML = '';
+    pantallaInicio.classList.add('oculto');
+    tablero.classList.remove('oculto');
+    generarTablero(movimientos);
+});
+
+btnIntermedio.addEventListener('click', () => {
+    movimientos = 22;
+    pantallaInicio.innerHTML = '';
+    pantallaInicio.classList.add('oculto');
+    tablero.classList.remove('oculto');
+    generarTablero(movimientos);
+});
+
+btnDificil.addEventListener('click', () => {
+    movimientos = 18;
+    //  pantallaInicio.innerHTML = '';
+    pantallaInicio.classList.add('oculto');
+    tablero.classList.remove('oculto');
+    generarTablero(movimientos);
+});
 
 
-function generarTablero() {
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////
+
+function generarTablero(mov) {
 
     let totalFichas = 12;
     Fichas.sort(() => (0.5 - Math.random()));
     let primeraEleccion = 0;
     let primerMov = false;
     let segundaEleccion = 0;
-    let contadorMovimientos = 0;
+    let contadorMovimientos = 1;
     let parejasIguales = 0;
 
+    tablero.innerHTML = `
+    <header class="resultado">
+        <h4 id="resultado"></h4>
+        <button class="oculto" id="play">Play Again</button>
+    </header>
+    `
+    const resultado = document.getElementById('resultado');
+    const btnPlay = document.getElementById('play');
+    tablero.appendChild(contenedor);
     for (let i = 1; i <= totalFichas; i++) {
         const ficha = document.createElement('div');
 
@@ -61,13 +115,17 @@ function generarTablero() {
         contenedor.appendChild(ficha);
 
         ficha.addEventListener('click', () => {
-            contadorMovimientos++;
+            if (contadorMovimientos == mov) {
+                resultado.innerText = '';
+                movimientos = 0;
+                gameOver();
+            }
+            // contadorMovimientos++;
             ficha.classList.remove('gris');
             ficha.classList.add(Fichas[i - 1].color);
+            ficha.classList.add('seleccionado')
 
             resultado.innerText = contadorMovimientos;
-
-
 
             if (primerMov == false) {
                 primeraEleccion = Fichas[i - 1].id;
@@ -77,7 +135,9 @@ function generarTablero() {
                 segundaEleccion = Fichas[i - 1].id;
                 console.log('Segunda Elección: ' + segundaEleccion);
                 primerMov = compararElecciones(primeraEleccion, segundaEleccion);
+                contadorMovimientos++;
                 if (primerMov) {
+                    ficha.classList.remove('seleccionado')
                     setTimeout(() => {
                         ficha.classList.add('gris');
                         ficha.classList.remove(Fichas[i - 1].color);
@@ -97,10 +157,13 @@ function generarTablero() {
 
                     } else {
                         parejasIguales++;
-                        console.log(parejasIguales)
+                        console.log(parejasIguales);
                     }
+                } else if (fichasIguales) {
+                    console.log('No puedes clickear la misma ficha')
                 }
-                //primerMov = false;
+
+
             }
 
 
@@ -109,15 +172,29 @@ function generarTablero() {
     }
 }
 
-generarTablero();
 
 function compararElecciones(uno, dos) {
     if (uno == dos) {
-        console.log('Son Iguales')
+        console.log('Pareja Correcta')
         return false
     } else {
         console.log('Equivocado');
         return true;
     }
 
+}
+
+function gameOver() {
+    console.log('abrir modal');
+    Modal.classList.add('on')
+    const btnModal = document.getElementById('btn-modal');
+    btnModal.addEventListener('click', () => {
+        contenedor.innerHTML = '';
+        tablero.innerHTML = '';
+        tablero.classList.add('oculto');
+        Modal.classList.remove('on');
+        pantallaInicio.classList.remove('oculto');
+        generarTablero(contadorMovimientos);
+
+    })
 }
